baf2a4f0cdd1f91f927330d0e19cdd0fc72ff87a
abbydev72 <abbydev72@gmail.com>
2021-05-11 02:17:14 -0700

    finished working shell (barebones)

--- shell.c
+++ shell.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "sys/types.h"
+#include <unistd.h>
+#include <string.h>
+#include "sys/wait.h"
 
 #define ASH_TOK_BUFFER_SIZE 64
 #define ASH_TOK_DELIM " \t\r\n\a"
 #define ASH_RL_BUFFER_SIZE 1024
 
+// prototypes
+void 	ash_loop(void);
+char 	*ash_read_line(void);
+char 	**ash_split_line(char *);
+int 	ash_launch(char **);
+int 	ash_num_builtins();
+int 	ash_cd(char**);
+int 	ash_help(char **);
+int 	ash_exit(char **);
+int 	ash_execute(char **);
+
+// built-in shell commands
+int 	ash_cd(char **args);
+int 	ash_help(char **args);
+int 	ash_exit(char **args);
+
 int main(int argc, char **argv) 
 {
-
-	return 0;
+	ash_loop();
+	return EXIT_SUCCESS;
 }
 
 void ash_loop(void) {
@@ -53,7 +72,7 @@ char *ash_read_line(void) {
 		c = getchar();
 
 		// if we hit EOL, replace it with NULL character and return
-		if(c == EOF || c = "\n") {
+		if(c == EOF || c == '\n') {
 			buffer[position] = '\0';
 			return buffer;
 		} else {
@@ -77,7 +96,7 @@ char *ash_read_line(void) {
 char **ash_split_line(char *line) {
 	int buffer_size = ASH_TOK_BUFFER_SIZE, position = 0;
 	char **tokens = malloc(buffer_size * sizeof(char*));
-	char **token;
+	char *token;
 
 	if(!tokens) {
 		fprintf(stderr, "ash: allocation error.\n");
@@ -118,7 +137,7 @@ int ash_launch(char **args) {
 	pid = fork();
 	if(pid == 0) {
 		// child process
-		if(execvp(args[0], args) ==) {
+		if(execvp(args[0], args) == 0) {
 			perror("ash");
 		} else {
 			// parent process
@@ -128,4 +147,65 @@ int ash_launch(char **args) {
 		}
 	}
 	return 1;
+}
+
+char *builtin_str[] = {
+	"cd",
+	"help",
+	"exit"
+};
+
+int (*builtin_func[]) (char **) = {
+	&ash_cd,
+	&ash_help,
+	&ash_exit
+};
+
+int ash_num_builtins() {
+	return sizeof(builtin_str) / sizeof(char *);
+}
+
+// builtin implementations
+int ash_cd(char **args) {
+	if(args[1] == NULL) {
+		fprintf(stderr, "ash: expected argument to \"cd\"\n");
+	} else {
+		if(chdir(args[1]) != 0) {
+			perror("ash");
+		}
+	}
+	return 1;
+}
+
+int ash_help(char **args) {
+	int i;
+	printf("Abby's Version of LSH, by Stephen Brennan.\n");
+	printf("Type program names and arguments, and then hit enter!\n");
+	printf("The following are built-in commands: \n");
+
+	for(i = 0; i < ash_num_builtins(); i++) {
+		printf(" %s\n", builtin_str[i]);
+	}
+
+	printf("Use the man command for information on other programs.\n");
+	return 1;
+}
+
+int ash_exit(char **args) {
+	return 0;
+}
+
+int ash_execute(char **args) {
+	int i;
+	if(args[0] == NULL) {
+		// empty command was entered
+		return 1;
+	}
+
+	for(i = 0; i < ash_num_builtins(); i++) {
+		if(strcmp(args[0], builtin_str[i]) == 0) {
+			return (*builtin_func[i])(args);
+		}
+	}
+	return ash_launch(args);
 }
